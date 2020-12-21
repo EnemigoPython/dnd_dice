6d7d199767ca52ea038386e45381a558816cdd91
EnemigoPython <70324566+EnemigoPython@users.noreply.github.com>
2020-12-21 17:28:15 +0000

    Update main.py
    21/12/20: added feature, better regex code structure

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import random
 import re
 
-pattern = re.compile(r'(\d+)?\s?d(4|6|8|10|12|20|100)')
+pattern = re.compile(r'(\d+)?\s?d(4|6|8|10|12|20|100)(\s)?([+]\s?\d+)?')
 
 while True:
-    user = input('Roll the dice, "h" for help\n')
-    x = pattern.match(user)
+    user = input('Roll the dice, "h" for help:\n')
+    x = pattern.search(user)
     if x is not None:
-        if user[0].isnumeric():
-            total = 0
-            for roll in range(int(user.split('d')[0])):
-                roll_val = random.randint(1, int(user.split('d')[1]))
-                print(roll_val)
-                total += roll_val
-            print(f'total: {total}')
+        match = x.group()
+        if '+' in match:
+            add = int(match.split('+')[1])
+            match = match.split('+')[0]
         else:
-            print(random.randint(1, int(user.split('d')[1])))
+            add = 0
+        if match[0].isnumeric():
+            rolls = int(match.split('d')[0])
+        else:
+            rolls = 1
+        total = 0
+        for roll in range(rolls):
+            roll_val = random.randint(1, int(match.split('d')[1]))
+            if add:
+                print(f'{roll_val} (+ {add} = {roll_val + add})')
+            else:
+                print(roll_val)
+            total += roll_val
+        if rolls > 1:
+            if add:
+                print(f'total: {total} (+ {add} = {total + add}, + {add * rolls} = {total + (add * rolls)})')
+            else:
+                print(f'total: {total}')
     elif user.lower() == 'h':
-        print('Type the amount of dice, followed by "d", followed by a dice type')
-        print('Dice types: 4, 6, 8, 10, 12, 20, 100')
+        print('Type the amount of dice, followed by "d", followed by a dice type.')
+        print('Optionally add "+" to add a number to your roll.')
+        print('Example roll: "2d20+5')
+        print('Dice types: 4, 6, 8, 10, 12, 20, 100.')
     else:
         print('Invalid roll, try again.')
+    print()
